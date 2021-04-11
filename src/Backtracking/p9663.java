b7117951bb084b9914d67a972253756ff1c574a3
Nayoung <ahenaka9@gmail.com>
2021-04-11 23:44:48 +0900

    Solve 9663

--- src/Backtracking/p9663.java
+++ src/Backtracking/p9663.java
@@ -1,2 +1,43 @@
+import java.util.Scanner;
+
 public class p9663 {
+    public static int[] board;
+    public static int N;
+    public static int result;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+
+        board = new int[N];
+
+        result = 0;
+
+        dfs(0);
+
+        System.out.println(result);
+    }
+
+    public static void dfs(int depth) {
+        if (depth == N) {
+            result++;
+        } else {
+            for (int i = 0; i < N; i++) {
+                if (checkBoard(depth, i)) {
+                    board[depth] = i;
+                    dfs(depth+1);
+                }
+            }
+        }
+    }
+
+    public static boolean checkBoard(int depth, int x) {
+        for (int i = 0; i < depth; i++) {
+            if (board[i] == x) return false;
+            if (Math.abs(depth - i) == Math.abs(x - board[i])) return false;
+        }
+
+        return true;
+    }
 }
