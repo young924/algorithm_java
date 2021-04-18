51788dcf2867353baf0cc3e8ff8a177a72ac39c6
Nayoung <ahenaka9@gmail.com>
2021-04-18 22:36:03 +0900

    Solve 14503

--- src/p14503.java
+++ src/p14503.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 
 public class p14503 {
     public static int N, M, r, c, d;
-    public static int[] dx = {-1, 0, 1, 0};
-    public static int[] dy = {0, 1, 0, -1};	// 0, 1, 2, 3일 때 왼  // 도는 건 역
+    public static int[] dx = {0, -1, 0, 1};
+    public static int[] dy = {-1, 0, 1, 0};	// 0, 1, 2, 3일 때 왼쪽
     public static int[][] board;
+    public static boolean[][] cleaned;
+    public static int numOfBlock = 1;
+    public static int x;
+    public static int y;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -18,6 +22,7 @@ public class p14503 {
         d = sc.nextInt();	// 0 북, 1 동, 2 남, 3 서
 
         board = new int[N][M];
+        cleaned = new boolean[N][M];
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
@@ -25,15 +30,68 @@ public class p14503 {
             }
         }
 
-        search(r, c);
+        cleaned[r][c] = true;
+
+        x = r;
+        y = c;
+
+        search();
+
+        System.out.println(numOfBlock);
     }
 
-    public static void search(int x, int y) {
-        /*
-         * 1) d = 0
-         * 왼쪽으로 회전 d -= 1
-         *
-         *
-         */
+    public static void search() {
+
+        while (true) {
+            boolean canClean = false;
+
+            for (int i = d; i > d-4; i--) {
+                int idx = (i + 4) % 4;
+                int nX = x + dx[idx];
+                int nY = y + dy[idx];
+
+                if (board[nX][nY] == 0 && !cleaned[nX][nY]) {
+                    switch (idx) {
+                        case 0 -> d = 3;
+                        case 1 -> d = 0;
+                        case 2 -> d = 1;
+                        case 3 -> d = 2;
+                    }
+                    x = nX;
+                    y = nY;
+                    cleaned[x][y] = true;
+                    numOfBlock++;
+                    canClean = true;
+                    break;
+                }
+            }
+
+            if (!canClean) {
+                int backIdx = (d + 3) % 4;
+                int nX = x + dx[backIdx];
+                int nY = y + dy[backIdx];
+
+                if (board[nX][nY] == 0) {
+                    x = nX;
+                    y = nY;
+                } else {
+                    return;
+                }
+            }
+        }
+
+    }
+
+    public static void printBoard() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (cleaned[i][j]) {
+                    System.out.print(2+" ");
+                } else {
+                    System.out.print(board[i][j]+" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
