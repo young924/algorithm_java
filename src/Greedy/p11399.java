9043b0ac8611e9a1af34958ef008d5fb8df36a5e
Nayoung <ahenaka9@gmail.com>
2021-05-07 23:25:29 +0900

    Solve 11399

---
+++ src/Greedy/p11399.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class p11399 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bf.readLine());
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int[] P = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            P[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(P);
+
+        int sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            sum += P[i] * (N - i);
+        }
+
+        System.out.println(sum);
+    }
+}
