2f44175695aa345c88a8eecb3a10946122638b5c
Nayoung <ahenaka9@gmail.com>
2021-05-26 23:55:58 +0900

    Add 1912

---
+++ src/DP/p1912.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// 연속합
+
+public class p1912 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
