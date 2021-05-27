c7c3edf8a456eff95295f0dac110e2fbd3da357c
Nayoung <ahenaka9@gmail.com>
2021-05-27 19:59:33 +0900

    Solve 1912

--- src/DP/p1912.java
+++ src/DP/p1912.java
@@ -1,11 +1,36 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 // 연속합
 
 public class p1912 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0] = arr[0];
+
+        for (int i = 1; i < N; i++) {
+            dp[i] = Math.max(dp[i-1] + arr[i], arr[i]);
+        }
+
+        int result = Integer.MIN_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            result = Math.max(result, dp[i]);
+        }
+
+        System.out.println(result);
     }
 }
