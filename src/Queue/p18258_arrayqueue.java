84e95fe1f3a921ee4688d8f102e94bf6390a368f
Nayoung <ahenaka9@gmail.com>
2021-06-04 17:07:13 +0900

    Solve 18258 using ArrayQueue

---
+++ src/Queue/p18258_arrayqueue.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+// 큐 2 (ArrayQueue를 이용한 구현)
+
+public class p18258_arrayqueue {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        Deque<Integer> q = new ArrayDeque<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            switch (st.nextToken()) {
+                case "push":
+                    q.offer(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    Integer item = q.poll();
+                    if (item == null) sb.append(-1).append("\n");
+                    else sb.append(item).append("\n");
+                    break;
+                case "size":
+                    sb.append(q.size()).append("\n");
+                    break;
+                case "empty":
+                    if (q.isEmpty()) sb.append(1).append("\n");
+                    else sb.append(0).append("\n");
+                    break;
+                case "front":
+                    Integer front = q.peek();
+                    if (front == null) sb.append(-1).append("\n");
+                    else sb.append(front).append("\n");
+                    break;
+                case "back":
+                    Integer back = q.peekLast();
+                    if (back == null) sb.append(-1).append("\n");
+                    else sb.append(back).append("\n");
+                    break;
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
